9d7884c6ad3ab8ed62ecb5c14ef7b8009502abfe
Aidan Clancy <fancyclancy@gmail.com>
2025-06-21 16:11:04 +0100

    Exercise - Show favourite icon on Movie Details page..

--- src/components/headerMovie/index.tsx
+++ src/components/headerMovie/index.tsx
@@ -1,43 +1,34 @@
 import React from "react";
-import ArrowBackIcon from "@mui/icons-material/ArrowBack";
-import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
-import Paper from "@mui/material/Paper";
-import IconButton from "@mui/material/IconButton";
 import Typography from "@mui/material/Typography";
-import HomeIcon from "@mui/icons-material/Home";
-import { MovieDetailsProps } from "../../types/interfaces"; 
+import CardHeader from "@mui/material/CardHeader";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import Grid from "@mui/material/Grid";
+import { BaseMovieProps } from "../../types/interfaces";
 
-const styles = {
-    root: {  
-    display: "flex",
-    justifyContent: "space-around",
-    alignItems: "center",
-    flexWrap: "wrap",
-    padding: 1.5,
-  },
-};
+interface HeaderMovieProps {
+  movie: BaseMovieProps;
+}
 
-const MovieHeader: React.FC<MovieDetailsProps> = (movie) => {
-  
+const HeaderMovie: React.FC<HeaderMovieProps> = ({ movie }) => {
+  const favouritesRaw = localStorage.getItem("favourites");
+  const favourites: BaseMovieProps[] = favouritesRaw ? JSON.parse(favouritesRaw) : [];
+
+  const isFavourite = favourites.some((m) => m.id === movie.id);
+
   return (
-    <Paper component="div" sx={styles.root}>
-      <IconButton aria-label="go back">
-        <ArrowBackIcon color="primary" fontSize="large" />
-      </IconButton>
-
-      <Typography variant="h4" component="h3">
-        {movie.title}{"   "}
-        <a href={movie.homepage}>
-          <HomeIcon color="primary"  fontSize="large"/>
-        </a>
-        <br />
-        <span>{`${movie.tagline}`} </span>
-      </Typography>
-      <IconButton aria-label="go forward">
-        <ArrowForwardIcon color="primary" fontSize="large" />
-      </IconButton>
-    </Paper>
+    <Grid container>
+      <Grid item xs={10}>
+        <CardHeader
+          title={
+            <Typography variant="h4" component="h3">
+              {movie.title}{" "}
+              {isFavourite && <FavoriteIcon color="secondary" fontSize="large" />}
+            </Typography>
+          }
+        />
+      </Grid>
+    </Grid>
   );
 };
 
-export default MovieHeader;
+export default HeaderMovie;
--- src/pages/homePage.tsx
+++ src/pages/homePage.tsx
@@ -10,14 +10,29 @@ const MovieListPage: React.FC = () => {
       m.id === movieId ? { ...m, favourite: true } : m
     );
     setMovies(updatedMovies);
+
+    const favourites = updatedMovies.filter((m) => m.favourite);
+    localStorage.setItem("favourites", JSON.stringify(favourites));
   };
 
   useEffect(() => {
+    const storedFavouritesRaw = localStorage.getItem("favourites");
+    const storedFavourites: BaseMovieProps[] = storedFavouritesRaw
+      ? JSON.parse(storedFavouritesRaw)
+      : [];
+
     fetch(
       `https://api.themoviedb.org/3/discover/movie?api_key=${import.meta.env.VITE_TMDB_KEY}&language=en-US&include_adult=false&page=1`
     )
       .then((res) => res.json())
-      .then((json) => setMovies(json.results));
+      .then((json) => {
+        const moviesWithFavourites = json.results.map((movie: BaseMovieProps) => {
+          const isFavourite = storedFavourites.some((fav) => fav.id === movie.id);
+          return isFavourite ? { ...movie, favourite: true } : movie;
+        });
+
+        setMovies(moviesWithFavourites);
+      });
   }, []);
 
   return (
